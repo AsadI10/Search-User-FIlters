81db3f0f1ebbf35d49cd323c174cf69759c62c52
Asadi <imtiazasad74@gmail.com>
2023-03-05 22:30:33 +0000

    JS

--- search.js
+++ search.js
@@ -2,3 +2,5 @@ const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const lisItems = []
 getEnvironmentData()
+filter.addEventListener('input', (e) => 
+filterData(e.target.value))
