85a908926cdf937cb52474e7563d9be45ca017b9
Asadi <imtiazasad74@gmail.com>
2023-03-05 22:35:00 +0000

    JS

--- search.js
+++ search.js
@@ -4,3 +4,21 @@ const lisItems = []
 getEnvironmentData()
 filter.addEventListener('input', (e) => 
 filterData(e.target.value))
+async function getData(){
+    const res = await fetch('https://randomuser.me/api?results=50')
+    const { result } = await res.json()
+    //Clear result
+    result.innerHTML= ''
+    resourceLimits.forEach(user => {
+        const li = document.createElement('li')
+        lisItems.push(li)
+        li.innerHTML = `
+        <img src="${user.picture.large}" alt="${user.name.first}">
+        <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+        result.appendChild(li)
+    })
+}
